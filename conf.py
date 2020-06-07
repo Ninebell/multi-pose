a113f2d6086561a4e4d5e922751641899358eb3b
Ninebell <ninebell94@gmail.com>
2020-06-07 17:43:40 +0900

    conflict

--- conf.py
+++ conf.py
@@ -1,11 +1,11 @@
 data_set_name = 'mpii'
 
-root_path = 'D:\\{0}'.format(data_set_name)
+root_path = 'E:\\dataset\\{0}'.format(data_set_name)
 train_path = "\\train\\"
 validate_path = "\\validate\\"
 
-heat_map_path = "\\heatmap\\"
-limb_path = "\\limb\\"
+heat_map_path = "\\heatmap_\\"
+limb_path = "\\limb_\\"
 #
 # train_annotation_path = '\\annotations\\person_keypoints_train2017.json'
 # val_annotation_path = '\\annotations\\person_keypoints_val2017.json'
