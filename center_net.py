e9a131a4aa2f8615366e83eafa71a49fff230fa3
Jonghoe <ninebell94@gmail.com>
2020-09-09 23:33:57 +0900

    conflict

--- center_net.py
+++ center_net.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 from conf import *
 
-from torch_module.utils import BaseDataLoader, train_model
+from torch_module.utils import train_model
 
 
 class CenterNet(nn.Module):
@@ -136,74 +136,6 @@ heat_map_path = "/heat/"
 limb_path = "/limb/"
 
 
-class CenterNetDataLoader(BaseDataLoader):
-    def __init__(self, batch_size, path, shuffle, repeat, is_cuda):
-        super(CenterNetDataLoader).__init__()
-        self.batch_size = batch_size
-        self.path = path
-        self.shuffle = shuffle
-        self.repeat = repeat
-        self.is_cuda = is_cuda
-
-    def data_load(self):
-        def concat_map(path, count):
-            file_path = path + '\\{}\\'.format(0) + file_name
-            base = np.asarray(Image.open(file_path)) / 255.
-            base = np.reshape(base, (64, 64, 1))
-
-            for h in range(1, count):
-                file_path = path + '\\{}\\'.format(h) + file_name
-                cc = np.asarray(Image.open(file_path)) / 255.
-                cc = np.reshape(cc, (64, 64, 1))
-                base = np.concatenate([base, cc], axis=-1)
-            return base
-
-        base_path = self.path
-        dirs = os.listdir(base_path + heat_map_path + '0')
-        if self.shuffle:
-            random.shuffle(dirs)
-        batch_iter = len(dirs) // self.batch_size
-        heat_map_len = len(os.listdir(base_path + heat_map_path))
-        limb_len = len(os.listdir(base_path + limb_path))
-        mean = np.array([0.485, 0.456, 0.406])
-        mean = np.array([np.ones((256, 256)) * m for m in mean])
-        std = np.array([0.229, 0.224, 0.225])
-        std = np.array([np.ones((256, 256)) * s for s in std])
-        mean = np.moveaxis(mean, 0, -1)
-        std = np.moveaxis(std, 0, -1)
-        for idx in range(batch_iter):
-            x = []
-            org_img = []
-            heat_maps = []
-            limbs = []
-
-            for b_i in range(self.batch_size):
-                file_name = dirs[idx * self.batch_size + b_i]
-                img = Image.open(base_path + '\\image\\' + file_name)
-                if img.mode == 'L':
-                    continue
-                # print(img.mode, file_name)
-                img = img.resize((256, 256))
-                org_img.append(img)
-                img = np.asarray(img) / 255.
-                img = (img - mean) / std
-                img = np.moveaxis(img, 2, 0)
-                x.append(img)
-
-                base_heat = concat_map(base_path + heat_map_path, heat_map_len)
-                base_limb = concat_map(base_path + limb_path, limb_len)
-
-                base_heat = np.moveaxis(base_heat, 2, 0)
-                base_limb = np.moveaxis(base_limb, 2, 0)
-                heat_maps.append(base_heat)
-                limbs.append(base_limb)
-
-            x = torch.from_numpy(np.asarray(x)).type(torch.FloatTensor).cuda()
-            heat_maps = torch.from_numpy(np.asarray(heat_maps)).type(torch.FloatTensor).cuda()
-            limbs = torch.from_numpy(np.asarray(limbs)).type(torch.FloatTensor).cuda()
-            yield x, [(heat_maps, limbs) for _ in range(self.repeat)]
-
-
 def center_net_loss(target, predict):
     total_loss = 0
     for layer in range(len(predict)):
@@ -220,15 +152,4 @@ if __name__ == "__main__":
     inp = torch.rand((1,3,256,256)).cuda()
     net.cuda()
     optim = torch.optim.Adam(net.parameters(), lr=1e-3)
-
-    cnd = CenterNetDataLoader(4, root_path+train_path, True, 3, True)
-
-    train_model(100, net, center_net_loss, optim, cnd, None, root_path+'/tensor_result')
-
-    result = net(inp)
-
-    for ret in result:
-        print(ret[0].shape)
-        print(ret[1].shape)
-
 
